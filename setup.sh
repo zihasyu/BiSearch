8abf328bd15ede80394e012134d0e4f676c82e19
zihasyu <1045839067@qq.com>
2024-07-10 11:47:20 +0800

    Modified some Cmakelists

--- setup.sh
+++ setup.sh
@@ -1,27 +1,56 @@
-# 获取脚本所在的绝对路径
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# # 获取脚本所在的绝对路径
+# SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# 完全删除并重新创建 build 目录
-if [ -d "$SCRIPT_DIR/build" ]; then
-    echo "Cleaning the build directory"
-    rm -rf "$SCRIPT_DIR/build"
+# # 完全删除并重新创建 build 目录
+# if [ -d "$SCRIPT_DIR/build" ]; then
+#     echo "Cleaning the build directory"
+#     rm -rf "$SCRIPT_DIR/build"
+# fi
+# echo "Creating the build directory"
+# mkdir -p "$SCRIPT_DIR/build"
+
+# # 进入 build 目录，运行 cmake 和 make
+# cd "$SCRIPT_DIR/build"
+# cmake ..
+# make -j4
+
+# # 清理 ./bin 目录下的 .txt 文件，并确保 Containers 和 restoreFile 目录存在
+# cd "$SCRIPT_DIR/bin"
+# rm -f *.txt
+# mkdir -p Containers restoreFile
+
+# # 如果 "./bin/Containers" 目录存在，则清理其内容
+# if [ -d "Containers" ]; then
+#     echo "Cleaning the Containers directory"
+#     rm -rf Containers/*
+#     echo "Done!"
+# fi
+
+if [ -d "build" ]; then
+    echo "clean the build directory"
+    rm -rf build/*
+    echo "Done!"
 fi
-echo "Creating the build directory"
-mkdir -p "$SCRIPT_DIR/build"
 
-# 进入 build 目录，运行 cmake 和 make
-cd "$SCRIPT_DIR/build"
+if [ ! -d "build" ]; then
+    echo "build the build directory"
+    mkdir -p build
+    echo "Done!"
+fi
+
+
+cd ./build
 cmake ..
 make -j4
+cd ..
+cd ./bin
+rm *.txt
+mkdir -p Containers
+mkdir -p restoreFile
 
-# 清理 ./bin 目录下的 .txt 文件，并确保 Containers 和 restoreFile 目录存在
-cd "$SCRIPT_DIR/bin"
-rm -f *.txt
-mkdir -p Containers restoreFile
-
-# 如果 "./bin/Containers" 目录存在，则清理其内容
+# Check if "./bin/Containers" directory exists and clear its content
 if [ -d "Containers" ]; then
-    echo "Cleaning the Containers directory"
+    echo "clean the Containers directory"
     rm -rf Containers/*
     echo "Done!"
 fi
