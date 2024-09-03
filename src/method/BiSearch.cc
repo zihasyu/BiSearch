af126b4b459d360efbc634977eaddb629e1b1981
zihasyu <hasyuziyu@gmail.com>
2024-09-03 13:38:27 +0800

    fix BiSearch-design 3 bug into locality windows

--- src/method/BiSearch.cc
+++ src/method/BiSearch.cc
@@ -123,7 +123,7 @@ void BiSearch::ProcessTrace()
 
                     // unique chunk & locality hit &locality can be accept
                     // tmpratio > LZ4_RATIO && tmpChunk.deltaFlag != NO_DELTA
-                    if (estimateGain(tmpChunk.chunkSize, tmpdeltachunksize)) //&&  ((plchunk.chunkType == FI && (tmpratio  >= plchunk.compressionRatio - FiOffset)) || plchunk.chunkType == DUP) )
+                    if (estimateGain(tmpChunk.chunkSize, tmpdeltachunksize) && tmpChunk.deltaFlag != NO_DELTA) //&&  ((plchunk.chunkType == FI && (tmpratio  >= plchunk.compressionRatio - FiOffset)) || plchunk.chunkType == DUP) )
                     {
                         tmpChunk.deltaFlag = LOCAL_DELTA;
                         tmpChunk.saveSize = tmpdeltachunksize;
