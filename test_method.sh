24e1f14de462a2a75980e8921b5a4f7e8c0b0aa9
zihasyu <hasyuziyu@gmail.com>
2024-09-03 16:05:56 +0800

    try design 3

--- test_method.sh
+++ test_method.sh
@@ -2,9 +2,9 @@ cd bin
 method=5
 chunking=4
 
-# name=LKT
-# num=84
-# ./BiSearch -i /mnt/dataset2/$name -c $chunking -m $method -n $num  >C"${chunking}M${method}_${name}".txt
+name=LKT
+num=84
+./BiSearch -i /mnt/dataset2/$name -c $chunking -m $method -n $num  >C"${chunking}M${method}_${name}".txt
 
 name=WEB
 num=102
