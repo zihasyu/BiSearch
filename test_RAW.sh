53530b41b0c1a88e5908218bc96ac9d69dda2671
zihasyu <1045839067@qq.com>
2025-01-12 14:58:59 +0800

    test_RAW

--- test_RAW.sh
+++ test_RAW.sh
@@ -1,23 +1,33 @@
 cd bin
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
+
 
 path=/mnt/dataset2/linux
 name=_linux
 num=270
 rm -r mTarFile
 mkdir mTarFile
-
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 6 -m 0 -n $num  >Mo_Dedup$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 7 -m 3 -n $num  >Mo_Odess$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 6 -m 4 -n $num  >Mo_Palantir$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 path=/mnt/dataset2/chromium
 name=_chromium
@@ -25,14 +35,20 @@ num=107
 ./BiSearch -i $path -c 6 -m 0 -n $num  >Mo_Dedup$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 7 -m 3 -n $num  >Mo_Odess$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 6 -m 4 -n $num  >Mo_Palantir$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 path=/mnt/dataset2/WEB
 name=_WEB
@@ -41,14 +57,20 @@ num=102
 ./BiSearch -i $path -c 6 -m 0 -n $num  >Mo_Dedup$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 7 -m 3 -n $num  >Mo_Odess$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 6 -m 4 -n $num  >Mo_Palantir$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 path=/mnt/dataset2/ThunderbirdTar
 name=_Thunderbird
@@ -57,11 +79,17 @@ num=240
 ./BiSearch -i $path -c 6 -m 0 -n $num  >Mo_Dedup$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 7 -m 3 -n $num  >Mo_Odess$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
 ./BiSearch -i $path -c 6 -m 4 -n $num  >Mo_Palantir$name.txt
 rm -r mTarFile
 mkdir mTarFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
