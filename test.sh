180187c3f80f2c2bb25daee7229362ec1c00d924
zihasyu <1045839067@qq.com>
2025-03-12 21:43:59 +0800

    updata shell

--- test.sh
+++ test.sh
@@ -1,126 +1,51 @@
 cd bin
+run_method(){
+  local path=$1
+  local name=$2
+  local num=$3
+./BiSearch -i $path -c 1 -m 0 -n $num  >Dedup$name.txt
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 
-path=/mnt/dataset2/linux
-name=_LKT
-num=270
-# # ./BiSearch -i $path -c 1 -m 0 -n $num  >Dedup$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# ./BiSearch -i $path -c 1 -m 1 -n $num  >Ntransform$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 2 -n $num  >Finesse$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
 ./BiSearch -i $path -c 1 -m 3 -n $num  >Odess$name.txt
 sudo rm Containers/*
 sudo echo 3 > /proc/sys/vm/drop_caches
+
 ./BiSearch -i $path -c 1 -m 4 -n $num  >Palantir$name.txt
 sudo rm Containers/*
 sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 5 -n $num  >BiSearch$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-# # ./BiSearch -i $path -c 1 -m 6 -n $num  >Locality$name.txt
-# ./BiSearch -i $path -c 5 -m 3 -n $num  >OdessMTar$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# # ./BiSearch -i $path -c 3 -m 5 -n $num  >BiSearchTarfast$name.txt
-./BiSearch -i $path -c 4 -m 5 -n $num  >BiSearchMultiTar$name.txt
+
+./BiSearch -i $path -c 5 -m 3 -n $num  >OdessMTar$name.txt
+sudo rm -r mTarFile
+sudo mkdir mTarFile
 sudo rm Containers/*
 sudo echo 3 > /proc/sys/vm/drop_caches
 
-
-path=/mnt/dataset2/chromium 
-name=_chromium
-num=107
-# ./BiSearch -i $path -c 1 -m 0 -n $num  >Dedup$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# ./BiSearch -i $path -c 1 -m 1 -n $num  >Ntransform$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 2 -n $num  >Finesse$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 3 -n $num  >Odess$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 4 -n $num  >Palantir$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 5 -n $num  >BiSearch$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-# ./BiSearch -i $path -c 1 -m 6 -n $num  >Locality$name.txt
-# ./BiSearch -i $path -c 5 -m 3 -n $num  >OdessMTar$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# ./BiSearch -i $path -c 3 -m 5 -n $num  >BiSearchTarfast$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 4 -m 5 -n $num  >BiSearchMultiTar$name.txt
+./BiSearch -i $path -c 5 -m 4 -n $num  >PalantirMTar$name.txt
+sudo rm -r mTarFile
+sudo mkdir mTarFile
 sudo rm Containers/*
 sudo echo 3 > /proc/sys/vm/drop_caches
 
-
-path=/mnt/dataset2/WEB
-name=_WEB
-num=102
-# # ./BiSearch -i $path -c 1 -m 0 -n $num  >Dedup$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# ./BiSearch -i $path -c 1 -m 1 -n $num  >Ntransform$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 2 -n $num  >Finesse$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 3 -n $num  >Odess$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 4 -n $num  >Palantir$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 5 -n $num  >BiSearch$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-# # ./BiSearch -i $path -c 1 -m 6 -n $num  >Locality$name.txt
-# ./BiSearch -i $path -c 5 -m 3 -n $num  >OdessMTar$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# # ./BiSearch -i $path -c 3 -m 5 -n $num  >BiSearchTarfast$name.txt
-./BiSearch -i $path -c 4 -m 5 -n $num  >BiSearchMultiTar$name.txt
+./BiSearch -i $path -c 4 -m 3 -n $num  >SA_Odess$name.txt
 sudo rm Containers/*
 sudo echo 3 > /proc/sys/vm/drop_caches
 
-path=/mnt/dataset2/ThunderbirdTar
-name=_Thunderbird
-num=240
-# # ./BiSearch -i $path -c 1 -m 0 -n $num  >Dedup$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# ./BiSearch -i $path -c 1 -m 1 -n $num  >Ntransform$name.txt
+./BiSearch -i $path -c 4 -m 5 -n $num  >SA_BiSearch$name.txt
 sudo rm Containers/*
 sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 2 -n $num  >Finesse$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 3 -n $num  >Odess$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 4 -n $num  >Palantir$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-./BiSearch -i $path -c 1 -m 5 -n $num  >BiSearch$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
-# # ./BiSearch -i $path -c 1 -m 6 -n $num  >Locality$name.txt
-# ./BiSearch -i $path -c 5 -m 3 -n $num  >OdessMTar$name.txt
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
-# # ./BiSearch -i $path -c 3 -m 5 -n $num  >BiSearchTarfast$name.txt
+}
 
-./BiSearch -i $path -c 4 -m 5 -n $num  >BiSearchMultiTar$name.txt
-sudo rm Containers/*
-sudo echo 3 > /proc/sys/vm/drop_caches
+run_method /mnt/dataset2/Android _Android 36
+run_method /mnt/dataset2/automake_tarballs _automake 100
+run_method /mnt/dataset2/bash_tarballs _bash 44
+run_method /mnt/dataset2/coreutils_tarballs _coreutils 28
+run_method /mnt/dataset2/fdisk_tarballs _fdisk 22
+run_method /mnt/dataset2/glibc_tarballs _glibc 100
+run_method /mnt/dataset2/smalltalk_tarballs _smalltalk 40
+run_method /mnt/dataset2/GNU_GCC/gcc-packed/tar _gcc 117
+run_method /mnt/dataset2/ThunderbirdTar _Thunderbird 240
+run_method /mnt/dataset2/chromium _chromium 107
+run_method /mnt/dataset2/linux _linux 270
+run_method /mnt/dataset2/WEB _WEB 102
+
