d9f6cf358085298a76f81a0dea173b3a8605336c
zihasyu <1045839067@qq.com>
2026-02-27 21:57:44 +0800

    update shell

--- test.sh
+++ test.sh
@@ -60,27 +60,27 @@ run_method(){
 # sudo rm Containers/*
 # sudo echo 3 > /proc/sys/vm/drop_caches
 
-./BiSearch -i $path -c 4 -m 5 -n $num  >SA_BiSearch$name.txt
+./BiSearch -i $path -c 4 -m 5 -n $num  -R 1 -L 0 >SA_BiSearch_deflate_Restore$name.txt
 # ./BiSearch -i $path -c 4 -m 3 -n $num  >SA_Ode$name.txt
-# sudo rm -r mtarRestore
-# sudo mkdir mtarRestore
-# sudo rm -r restoreFile
-# sudo mkdir restoreFile
-# sudo rm Containers/*
-# sudo echo 3 > /proc/sys/vm/drop_caches
+sudo rm -r mtarRestore
+sudo mkdir mtarRestore
+sudo rm -r restoreFile
+sudo mkdir restoreFile
+sudo rm Containers/*
+sudo echo 3 > /proc/sys/vm/drop_caches
 }
 
+run_method /mnt/dataset2/cross_c++_tar _cross_gcc_tar 317
 run_method /mnt/dataset2/react _react 100
 run_method /mnt/dataset2/netty  _netty 99
 run_method /mnt/dataset2/Cpython _Cpython 100
 run_method /mnt/dataset2/automake_tarballs _automake 100
-
 run_method /mnt/dataset2/coreutils_tarballs _coreutils 28
 run_method /mnt/dataset2/GNU_GCC/gcc-packed/tar _gcc 117
 run_method /mnt/dataset2/linux _linux 270
 run_method /mnt/dataset2/WEB _WEB 102
 
-run_method /mnt/dataset2/cross_c++_tar _cross_gcc_tar 317
+
 
 
 # run_method /mnt/dataset2/cross_gcc _cross_gcc 212
